db89e393cb3b370e9cf0a4621a610eb186ed875e
moses ngige <moses32ngigz@gmail.com>
2022-09-12 16:44:32 +0300

    server

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,56 @@
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  
+  # Add your routes here
+  get "/" do
+    { message: "Good luck with your project!" }.to_json
+  end
+  get "/doctors" do
+    doctors = Doctor.all
+    doctors.to_json
+  end
+
+  get "/patients" do
+    patients = Patient.all
+    patients.to_json
+  end
+
+  get "/appointments" do
+    appointments = Appointment.all
+    appointments.to_json(include:  [  :doctor,:patient]
+  )
+  end
+
+  get "/appointments/:id" do
+    appointment = Appointment.find(params[:id])
+    appointment.to_json
+  end
+
+  post "/appointments" do
+    appointment = Appointment.create(
+      appointment_date: params[:appointment_date],
+      appointment_duration: params[:appointment_duration],
+      appointment_reason: params[:appointment_reason],
+      appointment_type: params[:appointment_type],
+      doctor_id: params[:doctor_id],
+      patient_id: params[:patient_id],
+    )
+  end
+
+  patch "/appointments/:id" do
+    appointment = Appointment.find(params[:id])
+    appointment.update(
+      appointment_date: params[:appointment_date],
+      appointment_duration: params[:appointment_duration],
+      appointment_reason: params[:appointment_reason],
+      appointment_type: params[:appointment_type],
+    )
+  end
+
+  delete "/appointments/:id" do
+    appointment = Appointment.find(params[:id])
+    appointment.destroy
+    appointment.to_json
+  end
+
+end
